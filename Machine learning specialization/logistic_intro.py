fffb811165b393ad7e7d404b0097c9fae31f7fee
Ben Stager <bstager@tulane.edu>
2023-06-06 10:57:01 -0400

    logistic regression

---
+++ Machine learning specialization/logistic_intro.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+
+    return 1/(1 + np.exp(-x))
+
+# Test sigmoid function
+X = np.arange(-5,6)
+y = sigmoid(X)
+print(sigmoid(X))
+
+plt.plot(X, y)
+plt.show()
