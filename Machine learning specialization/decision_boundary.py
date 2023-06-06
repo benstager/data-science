c7d2312abc0a93a4f04cec88d04f81a8a2ee2e60
Ben Stager <bstager@tulane.edu>
2023-06-06 11:12:09 -0400

    testing decision boundary

---
+++ Machine learning specialization/decision_boundary.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
+
+plt.scatter(X[:,0],X[:,1])
+plt.show()
